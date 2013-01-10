77c9ff52d9e236e077d0d095638f2df29905a5bd
Lucy Park <525600min@gmail.com>
2013-01-11 00:36:35 +0900

    rm .gitconfig

--- .vim/skel/template.java
+++ .vim/skel/template.java
@@ -1,5 +1,5 @@
 public class Template {
     public static void main(String[] args) {
-        System.out.println('');
+        System.out.println("");
     }
 }
--- README.md
+++
@@ -1,4 +0,0 @@
-dotfiles
-========
-
-My dotfiles.
