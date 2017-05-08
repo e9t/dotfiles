ff26ac1e208909a7a624b35e934648c38c981119
Lucy Park <me@lucypark.kr>
2017-05-08 16:31:27 +0900

    Confirm init.sh several times and add timer

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # -----------------------------------------------
 # A shell script to init servers
 #
@@ -5,41 +7,54 @@
 # Created: 2017-05-08
 # -----------------------------------------------
 
+set -euo pipefail
 
 # FILLME ----------------------------------------
-HOMEC="$HOME"
+home="$HOME"
 install_dotfiles=1
 install_packages=1
 # -----------------------------------------------
 
-
 # confirm home
-read -r -p "Is \$HOME supposed to be \"$HOMEC\"? [y/N] " response
+read -r -p "Is \$HOME supposed to be \"$home\"? [y/N] " response
+if ! [[ $response == "y" ]]; then
+  read -r -p "Enter a new \$HOME: " home
+fi
+read -r -p "Are you sure about this? [y/N] " response
 if ! [[ $response == "y" ]]; then
   echo "Aborting..."
   exit 0
 fi
+read -r -p "This is your last warning. [y/N] " response
+if ! [[ $response == "y" ]]; then
+  echo "Aborting..."
+  exit 0
+fi
+for i in {5..1}; do
+    echo $i
+    sleep 1
+done
 
 # define paths
-DEV="$HOMEC/dev"
-
-# exit on fail
-set -e
+DEV="$home/dev"
 
 # start setup
 if [ $install_dotfiles -eq 1 ]; then
     echo "# Install dotfiles..."
-    cd $HOMEC
+    set -x  # start debug mode
+    cd $home
     git clone https://github.com/e9t/dotfiles.git
-    mv dotfiles/* dotfiles/.[^.]* $HOMEC
+    mv dotfiles/* dotfiles/.[^.]* $home
     rmdir dotfiles
-    sed -i "2s@.*@export HOME=\"$HOMEC\"@" $HOMEC/.bash_aliases
+    sed -i "2s@.*@export HOME=\"$home\"@" $home/.bash_aliases
     git submodule init
     git submodule update
+    set +x
 fi
 
 if [ $install_packages -eq 1 ]; then
     echo "# Install packages..."
+    set -x  # start debug mode
     mkdir -p $DEV
     cd $DEV
     git clone https://github.com/e9t/pkgs.git
@@ -47,9 +62,10 @@ if [ $install_packages -eq 1 ]; then
     git submodule init
     git submodule update
     git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-    cd $DEV/pkgs/fasd && PREFIX=$HOMEC make install
-    cd $DEV/pkgs/htop && ./autogen.sh && ./configure --prefix=$HOMEC && make && ln -s $PWD/htop $HOMEC/bin
-    cd $DEV/pkgs/tmux && ./autogen.sh && ./configure --exec_prefix="$HOMEC" && make && ln -s $PWD/tmux $HOMEC/bin
+    cd $DEV/pkgs/fasd && PREFIX=$home make install
+    cd $DEV/pkgs/htop && ./autogen.sh && ./configure --prefix=$home && make && ln -s $PWD/htop $home/bin
+    cd $DEV/pkgs/tmux && ./autogen.sh && ./configure --exec_prefix="$home" && make && ln -s $PWD/tmux $home/bin
+    set +x
 fi
 
 # vim:sw=4:ts=4:et
